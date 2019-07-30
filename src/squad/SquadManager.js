ab85a241bc9942bf124aa33032c8e8026fe87bbb
eddy <b.stanislawski@hotmail.com>
2019-07-30 10:01:14 +0200

    GRIDSTACK ADDED

--- src/squad/SquadManager.js
+++ src/squad/SquadManager.js
@@ -11,13 +11,21 @@ export default class SquadManager {
 
         const $pitch = this.$workspace.find('.pitch');
         $pitch.gridstack({
-            float: true
+            minWidth: 320,
+            width: 12,
+            cellHeight: 10,
+            verticalMargin: 0,
+            animate: true,
+            float: true,
+            resizable: {
+                handles: ''
+            }
         });
         const pitchGrid = $pitch.data('gridstack');
 
         this.$workspace.find('.add-player').click(() => {
             const playerHTML = elementWrapper({id: new Date().getTime()});
-            pitchGrid.addWidget($(playerHTML), undefined, undefined, 1, 1, true);
+            pitchGrid.addWidget($(playerHTML), undefined, undefined, 2, 6, true);
         });
     }
 }
