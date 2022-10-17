d32c76b5ad5a93d725eb02f60fcd7219cabaa243
eddyzzg <b.stanislawski@hotmail.com>
2022-10-17 21:15:30 +0200

    facegenerator and squadManager assigned for tests

--- src/match/Player.js
+++ src/match/Player.js
@@ -1,7 +1,18 @@
 export default class Player {
-    constructor(id, name, surname) {
-        this.id = '';
-        this.name = '';
-        this.surname = '';
+
+    constructor(id, name, surname, positionX, positionY, nominalPosition) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.speed = Math.ceil(Math.random()*100);
+        this.power = Math.ceil(Math.random()*100);
+        this.nominalPosition = nominalPosition;
+    }
+
+    move(positionX,positionY) {
+        positionX = positionX + 1;
+        positionY = positionY + 1;
     }
 }
